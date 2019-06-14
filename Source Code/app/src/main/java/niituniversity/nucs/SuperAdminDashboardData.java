8de942c2fb2a5e810785f336d594189733587db3
AJINKYA BEDEKAR <ajinkyabedekar2@gmail.com>
2019-06-14 18:51:08 +0530

    Add files via upload

---
+++ Source Code/app/src/main/java/niituniversity/nucs/SuperAdminDashboardData.java
@@ -0,0 +1,17 @@
+package niituniversity.nucs;
+
+public class SuperAdminDashboardData {
+    private String item;
+
+    public SuperAdminDashboardData(String item) {
+        this.item = item;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+}
